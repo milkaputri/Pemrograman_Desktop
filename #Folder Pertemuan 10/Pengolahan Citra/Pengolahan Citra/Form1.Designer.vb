cbbb48f505c6639203fa61aeafd7076967b006bf
Milka Putri <milka.putri@ti.ukdw.ac.id>
2025-05-06 09:08:38 +0700

    MINGGU 10

---
+++ #Folder Pertemuan 10/Pengolahan Citra/Pengolahan Citra/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmUtama
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        BukaToolStripMenuItem = New ToolStripMenuItem()
+        SimpanToolStripMenuItem = New ToolStripMenuItem()
+        PropertiToolStripMenuItem = New ToolStripMenuItem()
+        KeluarToolStripMenuItem = New ToolStripMenuItem()
+        HistogramToolStripMenuItem = New ToolStripMenuItem()
+        GreyscaleToolStripMenuItem = New ToolStripMenuItem()
+        CerahkanToolStripMenuItem = New ToolStripMenuItem()
+        GelapkanToolStripMenuItem = New ToolStripMenuItem()
+        TambahKontrasToolStripMenuItem = New ToolStripMenuItem()
+        KurangiKontrasToolStripMenuItem = New ToolStripMenuItem()
+        ResetToolStripMenuItem = New ToolStripMenuItem()
+        TampilkanHistogramToolStripMenuItem = New ToolStripMenuItem()
+        EfekToolStripMenuItem = New ToolStripMenuItem()
+        TajamkanToolStripMenuItem = New ToolStripMenuItem()
+        KaburkanToolStripMenuItem = New ToolStripMenuItem()
+        Putar90DerajatToolStripMenuItem = New ToolStripMenuItem()
+        FlipHorisontalToolStripMenuItem = New ToolStripMenuItem()
+        FlipVertikalToolStripMenuItem = New ToolStripMenuItem()
+        OpenFileDialog1 = New OpenFileDialog()
+        SaveFileDialog1 = New SaveFileDialog()
+        PictureBox1 = New PictureBox()
+        MenuStrip1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.ImageScalingSize = New Size(20, 20)
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, HistogramToolStripMenuItem, EfekToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 28)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {BukaToolStripMenuItem, SimpanToolStripMenuItem, PropertiToolStripMenuItem, KeluarToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(46, 24)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' BukaToolStripMenuItem
+        ' 
+        BukaToolStripMenuItem.Name = "BukaToolStripMenuItem"
+        BukaToolStripMenuItem.Size = New Size(224, 26)
+        BukaToolStripMenuItem.Text = "Buka"
+        ' 
+        ' SimpanToolStripMenuItem
+        ' 
+        SimpanToolStripMenuItem.Name = "SimpanToolStripMenuItem"
+        SimpanToolStripMenuItem.Size = New Size(224, 26)
+        SimpanToolStripMenuItem.Text = "Simpan"
+        ' 
+        ' PropertiToolStripMenuItem
+        ' 
+        PropertiToolStripMenuItem.Name = "PropertiToolStripMenuItem"
+        PropertiToolStripMenuItem.Size = New Size(224, 26)
+        PropertiToolStripMenuItem.Text = "Properti"
+        ' 
+        ' KeluarToolStripMenuItem
+        ' 
+        KeluarToolStripMenuItem.Name = "KeluarToolStripMenuItem"
+        KeluarToolStripMenuItem.Size = New Size(224, 26)
+        KeluarToolStripMenuItem.Text = "Keluar"
+        ' 
+        ' HistogramToolStripMenuItem
+        ' 
+        HistogramToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {GreyscaleToolStripMenuItem, CerahkanToolStripMenuItem, GelapkanToolStripMenuItem, TambahKontrasToolStripMenuItem, KurangiKontrasToolStripMenuItem, ResetToolStripMenuItem, TampilkanHistogramToolStripMenuItem})
+        HistogramToolStripMenuItem.Name = "HistogramToolStripMenuItem"
+        HistogramToolStripMenuItem.Size = New Size(93, 24)
+        HistogramToolStripMenuItem.Text = "Histogram"
+        ' 
+        ' GreyscaleToolStripMenuItem
+        ' 
+        GreyscaleToolStripMenuItem.Name = "GreyscaleToolStripMenuItem"
+        GreyscaleToolStripMenuItem.Size = New Size(233, 26)
+        GreyscaleToolStripMenuItem.Text = "Greyscale"
+        ' 
+        ' CerahkanToolStripMenuItem
+        ' 
+        CerahkanToolStripMenuItem.Name = "CerahkanToolStripMenuItem"
+        CerahkanToolStripMenuItem.Size = New Size(233, 26)
+        CerahkanToolStripMenuItem.Text = "Cerahkan"
+        ' 
+        ' GelapkanToolStripMenuItem
+        ' 
+        GelapkanToolStripMenuItem.Name = "GelapkanToolStripMenuItem"
+        GelapkanToolStripMenuItem.Size = New Size(233, 26)
+        GelapkanToolStripMenuItem.Text = "Gelapkan"
+        ' 
+        ' TambahKontrasToolStripMenuItem
+        ' 
+        TambahKontrasToolStripMenuItem.Name = "TambahKontrasToolStripMenuItem"
+        TambahKontrasToolStripMenuItem.Size = New Size(233, 26)
+        TambahKontrasToolStripMenuItem.Text = "Tambah Kontras"
+        ' 
+        ' KurangiKontrasToolStripMenuItem
+        ' 
+        KurangiKontrasToolStripMenuItem.Name = "KurangiKontrasToolStripMenuItem"
+        KurangiKontrasToolStripMenuItem.Size = New Size(233, 26)
+        KurangiKontrasToolStripMenuItem.Text = "Kurangi Kontras"
+        ' 
+        ' ResetToolStripMenuItem
+        ' 
+        ResetToolStripMenuItem.Name = "ResetToolStripMenuItem"
+        ResetToolStripMenuItem.Size = New Size(233, 26)
+        ResetToolStripMenuItem.Text = "Reset"
+        ' 
+        ' TampilkanHistogramToolStripMenuItem
+        ' 
+        TampilkanHistogramToolStripMenuItem.Name = "TampilkanHistogramToolStripMenuItem"
+        TampilkanHistogramToolStripMenuItem.Size = New Size(233, 26)
+        TampilkanHistogramToolStripMenuItem.Text = "Tampilkan Histogram"
+        ' 
+        ' EfekToolStripMenuItem
+        ' 
+        EfekToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {TajamkanToolStripMenuItem, KaburkanToolStripMenuItem, Putar90DerajatToolStripMenuItem, FlipHorisontalToolStripMenuItem, FlipVertikalToolStripMenuItem})
+        EfekToolStripMenuItem.Name = "EfekToolStripMenuItem"
+        EfekToolStripMenuItem.Size = New Size(51, 24)
+        EfekToolStripMenuItem.Text = "Efek"
+        ' 
+        ' TajamkanToolStripMenuItem
+        ' 
+        TajamkanToolStripMenuItem.Name = "TajamkanToolStripMenuItem"
+        TajamkanToolStripMenuItem.Size = New Size(224, 26)
+        TajamkanToolStripMenuItem.Text = "Tajamkan"
+        ' 
+        ' KaburkanToolStripMenuItem
+        ' 
+        KaburkanToolStripMenuItem.Name = "KaburkanToolStripMenuItem"
+        KaburkanToolStripMenuItem.Size = New Size(224, 26)
+        KaburkanToolStripMenuItem.Text = "Kaburkan"
+        ' 
+        ' Putar90DerajatToolStripMenuItem
+        ' 
+        Putar90DerajatToolStripMenuItem.Name = "Putar90DerajatToolStripMenuItem"
+        Putar90DerajatToolStripMenuItem.Size = New Size(224, 26)
+        Putar90DerajatToolStripMenuItem.Text = "Putar 90 derajat"
+        ' 
+        ' FlipHorisontalToolStripMenuItem
+        ' 
+        FlipHorisontalToolStripMenuItem.Name = "FlipHorisontalToolStripMenuItem"
+        FlipHorisontalToolStripMenuItem.Size = New Size(224, 26)
+        FlipHorisontalToolStripMenuItem.Text = "Flip Horisontal"
+        ' 
+        ' FlipVertikalToolStripMenuItem
+        ' 
+        FlipVertikalToolStripMenuItem.Name = "FlipVertikalToolStripMenuItem"
+        FlipVertikalToolStripMenuItem.Size = New Size(224, 26)
+        FlipVertikalToolStripMenuItem.Text = "Flip Vertikal"
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Dock = DockStyle.Fill
+        PictureBox1.Location = New Point(0, 28)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(800, 422)
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' frmUtama
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(PictureBox1)
+        Controls.Add(MenuStrip1)
+        Name = "frmUtama"
+        Text = "Pengolahan Citra"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BukaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SimpanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PropertiToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents KeluarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HistogramToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GreyscaleToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CerahkanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GelapkanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TambahKontrasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents KurangiKontrasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ResetToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TampilkanHistogramToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EfekToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TajamkanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents KaburkanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Putar90DerajatToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FlipHorisontalToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FlipVertikalToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ #Folder Pertemuan 10/Pengolahan Citra/Pengolahan Citra/Form1.vb
@@ -0,0 +1,274 @@
+﻿Public Class frmUtama
+    'Public namafile As String
+    Dim namafile As String
+
+    Private Sub BukaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BukaToolStripMenuItem.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.InitialDirectory = "c:\"
+        openFileDialog1.Filter = "Bitmap files (*.bmp)|*.bmp|JPG files (*.jpg)|*.jpg"
+        openFileDialog1.FilterIndex = 2
+        openFileDialog1.RestoreDirectory = True
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            PictureBox1.Image = Image.FromFile(openFileDialog1.FileName)
+            namafile = openFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub SimpanToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SimpanToolStripMenuItem.Click
+        If PictureBox1.Image Is Nothing Then
+            MessageBox.Show("Tidak ada gambar yang bisa disimpan. Silakan buka gambar terlebih dahulu.")
+            Exit Sub
+        End If
+
+        Dim saveFileDialog1 As New SaveFileDialog()
+        Dim MyPicture As Image
+        MyPicture = PictureBox1.Image
+        saveFileDialog1.Filter = "Bitmap files (*.bmp)|*.bmp|JPG files (*.jpg)|*.jpg"
+        saveFileDialog1.FilterIndex = 2
+        saveFileDialog1.RestoreDirectory = True
+        If saveFileDialog1.ShowDialog() = DialogResult.OK Then
+            If saveFileDialog1.FilterIndex = 1 Then
+                MyPicture.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp)
+            End If
+            If saveFileDialog1.FilterIndex = 2 Then
+                MyPicture.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg)
+            End If
+        End If
+    End Sub
+
+    Private Sub PropertiToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PropertiToolStripMenuItem.Click
+        MessageBox.Show("Nama File: " + namafile + vbCr + "Lebar: " +
+        PictureBox1.Image.Width.ToString + vbCr + "Tinggi: " +
+        PictureBox1.Image.Height.ToString)
+    End Sub
+
+    Private Sub GreyscaleToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GreyscaleToolStripMenuItem.Click
+        Dim r, g, b, gray As Integer
+        Dim bmp = New Bitmap(PictureBox1.Image)
+        For bar As Integer = 0 To PictureBox1.Image.Height - 1
+            For kol As Integer = 0 To PictureBox1.Image.Width - 1
+                r = bmp.GetPixel(kol, bar).R
+                g = bmp.GetPixel(kol, bar).G
+                b = bmp.GetPixel(kol, bar).B
+                gray = Math.Round(0.2126 * r + 0.7152 * g + 0.0722 * b)
+                bmp.SetPixel(kol, bar, Color.FromArgb(gray, gray, gray))
+            Next
+        Next
+        'Dim img As Image
+        'img = CType(bmp, Image)
+        'PictureBox1.Image = img
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub CerahkanToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CerahkanToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim bmp = New Bitmap(PictureBox1.Image)
+        For bar As Integer = 0 To PictureBox1.Image.Height - 1
+            For kol As Integer = 0 To PictureBox1.Image.Width - 1
+                r = bmp.GetPixel(kol, bar).R + 10
+                g = bmp.GetPixel(kol, bar).G + 10
+                b = bmp.GetPixel(kol, bar).B + 10
+                If r > 255 Then r = 255
+                If g > 255 Then g = 255
+                If b > 255 Then b = 255
+                bmp.SetPixel(kol, bar, Color.FromArgb(r, g, b))
+            Next
+        Next
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub GelapkanToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GelapkanToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim bmp = New Bitmap(PictureBox1.Image)
+        For bar As Integer = 0 To PictureBox1.Image.Height - 1
+            For kol As Integer = 0 To PictureBox1.Image.Width - 1
+                r = bmp.GetPixel(kol, bar).R - 10
+                g = bmp.GetPixel(kol, bar).G - 10
+                b = bmp.GetPixel(kol, bar).B - 10
+                If r < 0 Then r = 0
+                If g < 0 Then g = 0
+                If b < 0 Then b = 0
+                bmp.SetPixel(kol, bar, Color.FromArgb(r, g, b))
+            Next
+        Next
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub TambahKontrasToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TambahKontrasToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim bmp = New Bitmap(PictureBox1.Image)
+        For bar As Integer = 0 To PictureBox1.Image.Height - 1
+            For kol As Integer = 0 To PictureBox1.Image.Width - 1
+                r = bmp.GetPixel(kol, bar).R
+                g = bmp.GetPixel(kol, bar).G
+                b = bmp.GetPixel(kol, bar).B
+                r = Math.Round(128 + (1.1 * (r - 128)))
+                g = Math.Round(128 + (1.1 * (g - 128)))
+                b = Math.Round(128 + (1.1 * (b - 128)))
+                If r < 0 Then r = 0
+                If g < 0 Then g = 0
+                If b < 0 Then b = 0
+                If r > 255 Then r = 255
+                If g > 255 Then g = 255
+                If b > 255 Then b = 255
+                bmp.SetPixel(kol, bar, Color.FromArgb(r, g, b))
+            Next
+        Next
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub KurangiKontrasToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KurangiKontrasToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim bmp = New Bitmap(PictureBox1.Image)
+        For bar As Integer = 0 To PictureBox1.Image.Height - 1
+            For kol As Integer = 0 To PictureBox1.Image.Width - 1
+                r = bmp.GetPixel(kol, bar).R
+                g = bmp.GetPixel(kol, bar).G
+                b = bmp.GetPixel(kol, bar).B
+                r = Math.Round(128 + (0.90909 * (r - 128)))
+                g = Math.Round(128 + (0.90909 * (g - 128)))
+                b = Math.Round(128 + (0.90909 * (b - 128)))
+                If r < 0 Then r = 0
+                If g < 0 Then g = 0
+                If b < 0 Then b = 0
+                If r > 255 Then r = 255
+                If g > 255 Then g = 255
+                If b > 255 Then b = 255
+                bmp.SetPixel(kol, bar, Color.FromArgb(r, g, b))
+            Next
+        Next
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub ResetToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ResetToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim bmp = New Bitmap(namafile)
+        For bar As Integer = 0 To PictureBox1.Image.Height - 1
+            For kol As Integer = 0 To PictureBox1.Image.Width - 1
+                r = bmp.GetPixel(kol, bar).R
+                g = bmp.GetPixel(kol, bar).G
+                b = bmp.GetPixel(kol, bar).B
+                bmp.SetPixel(kol, bar, Color.FromArgb(r, g, b))
+            Next
+        Next
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub TampilkanHistogramToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TampilkanHistogramToolStripMenuItem.Click
+        frmHistogram.ShowDialog()
+    End Sub
+
+    Private Sub TajamkanToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TajamkanToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim bmp = New Bitmap(PictureBox1.Image)
+
+        ' Kernel Penajaman (Laplacian)
+        Dim kernel As Integer() = {-1, -1, -1, -1, 8, -1, -1, -1, -1}
+        Dim pembagi As Integer = 3 ' atau bisa 1 jika tidak ingin melemahkan efek
+
+        ' Kernel Emboss
+        ' Dim kernel As Integer() = {-2, -1, 0, -1, 1, 1, 0, 1, 2}
+        ' Dim pembagi As Integer = 1
+
+        ' Kernel Gaussian Blur
+        ' Dim kernel As Integer() = {1, 2, 1, 2, 4, 2, 1, 2, 1}
+        ' Dim pembagi As Integer = 16
+
+        ' Kernel Sobel X (Deteksi Tepi Horizontal)
+        '  Dim kernel As Integer() = {-1, 0, 1, -2, 0, 2, -1, 0, 1
+        ' Dim pembagi As Integer = 1
+
+        For bar As Integer = 1 To bmp.Height - 2
+            For kol As Integer = 1 To bmp.Width - 2
+                r = 0 : g = 0 : b = 0
+                For i As Integer = 0 To 8
+                    Dim px = bmp.GetPixel(kol - 1 + (i Mod 3), bar - 1 + (i \ 3))
+                    r += kernel(i) * px.R
+                    g += kernel(i) * px.G
+                    b += kernel(i) * px.B
+                Next
+
+                r = Math.Floor(r / pembagi)
+                g = Math.Floor(g / pembagi)
+                b = Math.Floor(b / pembagi)
+
+                ' Clamp nilai warna
+                r = Math.Min(255, Math.Max(0, r))
+                g = Math.Min(255, Math.Max(0, g))
+                b = Math.Min(255, Math.Max(0, b))
+
+                bmp.SetPixel(kol, bar, Color.FromArgb(r, g, b))
+            Next
+        Next
+
+        PictureBox1.Image = bmp
+    End Sub
+
+
+    Private Sub KaburkanToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KaburkanToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim bmp = New Bitmap(PictureBox1.Image)
+        Dim kernel As Integer() = {1, 1, 1, 1, 1, 1, 1, 1, 1}
+        For bar As Integer = 1 To PictureBox1.Image.Height - 2
+            For kol As Integer = 1 To PictureBox1.Image.Width - 2
+                r = 0
+                g = 0
+                b = 0
+                For i As Integer = 0 To 8
+                    r = r + (kernel(i) * bmp.GetPixel(kol - 1 + (i Mod 3), bar - 1 + (i \ 3)).R)
+                    g = g + (kernel(i) * bmp.GetPixel(kol - 1 + (i Mod 3), bar - 1 + (i \ 3)).G)
+                    b = b + (kernel(i) * bmp.GetPixel(kol - 1 + (i Mod 3), bar - 1 + (i \ 3)).B)
+                Next
+                r = Math.Floor(r / 9)
+                g = Math.Floor(g / 9)
+                b = Math.Floor(b / 9)
+                If r < 0 Then r = 0
+                If g < 0 Then g = 0
+                If b < 0 Then b = 0
+                If r > 255 Then r = 255
+                If g > 255 Then g = 255
+                If b > 255 Then b = 255
+                bmp.SetPixel(kol, bar, Color.FromArgb(r, g, b))
+            Next
+        Next
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub Putar90DerajatToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Putar90DerajatToolStripMenuItem.Click
+        Dim bmp = New Bitmap(PictureBox1.Image)
+        bmp.RotateFlip(RotateFlipType.Rotate90FlipNone)
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub FlipHorisontalToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FlipHorisontalToolStripMenuItem.Click
+        If PictureBox1.Image Is Nothing Then
+            MessageBox.Show("Silakan buka gambar terlebih dahulu.")
+            Exit Sub
+        End If
+
+        Dim srcBmp As New Bitmap(PictureBox1.Image)
+        Dim width As Integer = srcBmp.Width
+        Dim height As Integer = srcBmp.Height
+        Dim flippedBmp As New Bitmap(width, height)
+
+        For y As Integer = 0 To height - 1
+            For x As Integer = 0 To width - 1
+                ' Salin pixel dari kanan ke kiri (mirror horizontal)
+                flippedBmp.SetPixel(width - 1 - x, y, srcBmp.GetPixel(x, y))
+            Next
+        Next
+
+        PictureBox1.Image = flippedBmp
+    End Sub
+
+
+    Private Sub FlipVertikalToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FlipVertikalToolStripMenuItem.Click
+        Dim bmp = New Bitmap(PictureBox1.Image)
+        bmp.RotateFlip(RotateFlipType.RotateNoneFlipY)
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+End Class
